7652199a0bcdf8dc3f7501460910961f45b4f938
magneum-II <yt-dlx@proton.me>
2025-01-23 12:59:22 +0530

    picWall

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const config = getDefaultConfig(__dirname);
-module.exports = withNativeWind(config, { input: "./src/global.css" });
+const { getDefaultConfig } = require("expo/metro-config");
+module.exports = withNativeWind(getDefaultConfig(__dirname), { input: "./src/global.css" });
