bb7336791afd1db399881b3e28095371aae19076
magneum-II <160623192+yt-dlx@users.noreply.github.com>
2025-01-23 13:27:17 +0530

    Delete server directory

--- server/src/app/api/database/route.ts
+++
@@ -1,7 +0,0 @@
-// src/app/api/database/route.ts
-import metaBase from "@/database";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json(metaBase);
-}
--- server/src/app/api/version/route.ts
+++
@@ -1,6 +0,0 @@
-// src/app/api/version/route.ts
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({ version: "4.0.0" });
-}
