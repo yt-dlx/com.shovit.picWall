620619652a1ce4f08b89748fc3e4c82f4f9672b0
magneum-II <160623192+yt-dlx@users.noreply.github.com>
2025-01-23 13:28:56 +0530

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const { withNativeWind } = require("nativewind/metro");
-const { getDefaultConfig } = require("expo/metro-config");
-module.exports = withNativeWind(getDefaultConfig(__dirname), { input: "./src/global.css" });
