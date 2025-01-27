2c15bef28a7151d4895f2e4ab7f0d60f895c0a4f
magneum-II <yt-dlx@proton.me>
2025-01-27 18:57:51 +0530

    picWall

--- core/proc/solo/Script_0.py
+++ core/proc/solo/Script_0.py
@@ -1,21 +1,18 @@
 #=======================================================================================================================================================================================
-# Script 0: Create the Original Folders
+# Script 0: Create the Opposite Folder Structure
 #=======================================================================================================================================================================================
 import os
-main_folder = "image"
-sub_topics = [
-    "Serene", "Urban", "Landscapes", "Geometry", "Abstract", "Minimalism", 
-    "Ethereal", "Nature", "Vintage", "Vehicles", "Cyberpunk"
-]
-models = ["Anime", "Cinematic", "Geometry"]
+main_folder = "input"
+models = ["Anime", "Cinematic", "Geometry", "Photography"]
+sub_topics = ["Abstract", "Aerial-View", "Cyberpunk", "Dusk-life", "Ethereal", "Landscapes", "Minimalism", "Nature", "Night-life", "Rain", "Season", "Serene", "Snow", "Synthwave", "Techno", "Urban", "Vehicles", "Vintage"]
 if not os.path.exists(main_folder):
     os.makedirs(main_folder)
-for sub_topic in sub_topics:
-    sub_topic_path = os.path.join(main_folder, sub_topic)
-    if not os.path.exists(sub_topic_path):
-        os.makedirs(sub_topic_path)
-    for model in models:
-        model_path = os.path.join(sub_topic_path, model)
-        if not os.path.exists(model_path):
-            os.makedirs(model_path)
-print("Folder structure created successfully!")
+for model in models:
+    model_path = os.path.join(main_folder, model)
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+    for sub_topic in sub_topics:
+        sub_topic_path = os.path.join(model_path, sub_topic)
+        if not os.path.exists(sub_topic_path):
+            os.makedirs(sub_topic_path)
+print("Opposite folder structure created successfully!")
