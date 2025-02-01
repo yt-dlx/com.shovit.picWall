6f34c1d6b386b341ae45f7bb4ca3e9f1ae31e282
magneum-II <yt-dlx@proton.me>
2025-02-01 22:06:26 +0530

    com.shovit.picWall

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,25 +5,14 @@ import { memo } from "react";
 import colorize from "@/utils/colorize";
 import { Text, View } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
-import { useWindowDimensions } from "react-native";
-/* ============================================================================================ */
-/* ============================================================================================ */
-const useResponsiveStyles = () => {
-  const { width } = useWindowDimensions();
-  const rf = (size: number, factor = 0.5) => {
-    const ratio = (size * width) / 390;
-    return size + (ratio - size) * factor;
-  };
-  return { rf };
-};
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 /* ============================================================================================ */
 /* ============================================================================================ */
 const Footer: React.FC = memo(() => {
-  const { rf } = useResponsiveStyles();
   return (
-    <View className="relative w-full" style={{ padding: rf(8), backgroundColor: colorize("#111111", 1.0) }}>
-      <Text style={{ textAlign: "center", fontSize: rf(14), fontFamily: "Lobster", color: colorize("#F4F4F5", 1.0) }}>
-        Crafted with <AntDesign name="heart" size={rf(15)} color={colorize("#FF000D", 1.0)} /> in India. All rights reserved{" "}
+    <View style={{ padding: wp(2), backgroundColor: colorize("#111111", 1.0) }}>
+      <Text style={{ textAlign: "center", fontSize: wp(4), fontFamily: "Lobster", color: colorize("#F4F4F5", 1.0) }}>
+        Crafted with <AntDesign name="heart" size={wp(5)} color={colorize("#FF000D", 1.0)} /> in India. All rights reserved
       </Text>
     </View>
   );
