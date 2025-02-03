f90d72256066aea2817ef59065ce460293b2d9ef
magneum-II <yt-dlx@proton.me>
2025-02-03 15:18:26 +0530

    com.shovit.picWall

--- src/app/Test/index.tsx
+++ src/app/Test/index.tsx
@@ -5,6 +5,7 @@ import { Image } from "expo-image";
 import { FC, useEffect } from "react";
 import colorize from "@/utils/colorize";
 import { Text, View } from "react-native";
+import Footer from "@/components/Footer";
 import { useSharedValue, withTiming } from "react-native-reanimated";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 /* ============================================================================================================================== */
@@ -18,18 +19,19 @@ const ErrorPage: FC = () => {
   return (
     <View
       style={{
-        backgroundColor: colorize("#171717", 1.0),
-        borderColor: colorize("#BE3025", 1.0),
-        borderBottomWidth: wp(1),
-        justifyContent: "flex-start",
-        borderRightWidth: wp(1),
-        borderLeftWidth: wp(1),
-        alignItems: "center",
-        position: "absolute",
-        bottom: 0,
-        right: 0,
+        flex: 1,
+        top: 0,
         left: 0,
-        top: 0
+        right: 0,
+        bottom: 0,
+        position: "relative",
+        alignItems: "center",
+        borderLeftWidth: wp(0.4),
+        borderRightWidth: wp(0.4),
+        justifyContent: "flex-start",
+        borderBottomWidth: wp(0.4),
+        borderColor: colorize("#BE3025", 1.0),
+        backgroundColor: colorize("#171717", 1.0)
       }}
     >
       <View style={{ position: "absolute", top: hp("10%"), left: 0, right: 0, justifyContent: "center", alignItems: "center" }}>
@@ -38,18 +40,14 @@ const ErrorPage: FC = () => {
           contentFit="contain"
           accessibilityLabel="picWallLogo"
           source={require("@/assets/images/logo.jpg")}
-          style={{
-            width: wp("40%"),
-            height: wp("40%"),
-            borderWidth: wp("0.5%"),
-            borderRadius: wp("20%"),
-            borderColor: colorize("#BE3025", 1.0)
-          }}
+          style={{ width: wp(40), height: wp(40), borderWidth: wp(0.5), borderRadius: wp(50), borderColor: colorize("#BE3025", 1.0) }}
         />
       </View>
-      <View style={{ alignItems: "center", marginTop: hp("30%") }}>
-        <Text style={{ margin: wp("2%"), fontSize: wp("12%"), fontFamily: "Lobster", color: colorize("#BE3025", 1.0), textDecorationLine: "underline" }}>Oops Error!</Text>
-        <View style={{ marginBottom: hp("2%"), alignItems: "center" }}></View>
+      <View style={{ alignItems: "center", marginTop: hp("20%") }}>
+        <Text style={{ marginTop: wp("20%"), fontSize: wp("12%"), fontFamily: "Lobster", color: colorize("#BE3025", 1.0), textDecorationLine: "underline" }}>Oops Error</Text>
+      </View>
+      <View style={{ position: "absolute", bottom: 0, left: 0, right: 0, zIndex: 1 }}>
+        <Footer />
       </View>
     </View>
   );
