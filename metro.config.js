7a9214bfe89e0e15d8f1a05bad578da73a62ae45
magneum-II <yt-dlx@proton.me>
2025-02-05 10:21:49 +0530

    com.client.picWall

--- metro.config.js
+++
@@ -1,3 +0,0 @@
-const { withNativeWind } = require("nativewind/metro");
-const { getDefaultConfig } = require("expo/metro-config");
-module.exports = withNativeWind(getDefaultConfig(__dirname), { input: "./global.css" });
